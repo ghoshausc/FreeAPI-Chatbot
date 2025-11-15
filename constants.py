b83f571edc33cff55c54186afe4368920bf68708
Anindita Ghosh <97768770+ghoshausc@users.noreply.github.com>
2025-11-15 14:27:12 -0800

    File containing all LLM API keys
    File containing all LLM API keys

---
+++ constants.py
@@ -0,0 +1,15 @@
+OPENAI_COST_FILE = "GPT_COST.txt"
+
+GPT_MODEL_NAME_FOR_JSON = "gpt-4o-mini"
+
+TOP_K = 50   ####### Can be increased/decreased any time!
+
+OPENAI_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxx"  
+
+GROQ_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxx"
+
+CEREBRAS_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxx"
+
+EMBEDDING_MODEL_NAME = 'paraphrase-MiniLM-L6-v2'
+
+######### for more accurate embeddings, use thenlper-gte/large. this takes more time
